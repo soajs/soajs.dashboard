3829849df6a7bda3b75ff43889078688af09e7ee
mikehajj <mike.hajj@gmail.com>
2015-09-10 18:40:37 +0300

    bug fix in upload folder path

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
         "dashboardFolderName": "/dashboard/",
         "servicesFolderName": "/services/",
         "uiFolderName": "/ui/",
-        "uploadFolderName": "uploads/",
+        "uploadFolderName": "/uploads/",
         "tenantFolderName": "/tenants/"
     },
 
