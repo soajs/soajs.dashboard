1f26fdf33b0ae3a21838a045b5230f11205dbd43
ameer <faraj.ameer@gmail.com>
2016-10-13 13:12:59 +0300

    SOAJ-365: added logging for testing purposes

--- utils/drivers/container/docker.js
+++ utils/drivers/container/docker.js
@@ -95,8 +95,15 @@ var lib = {
 					async.detect(managerNodes, function (oneNode, callback) {
 						var dockerConfig = buildDockerConfig(oneNode.ip, oneNode.dockerPort, certs);
 						var docker = new Docker(dockerConfig);
-						return docker.ping(callback);
+						return docker.ping(function (error, response) {
+							if (error) console.log ('Error: ' + error);
+							checkError(error, callback, function () {
+								console.log ('Response: ' + response);
+								return callback(null, response);
+							});
+						});
 					}, function (error, fastestNodeRecord) {
+						console.log ('Selected node: ' + fastestNodeRecord.name);
 						checkError(error, cb, function () {
 							var dockerConfig = buildDockerConfig(fastestNodeRecord.ip, fastestNodeRecord.dockerPort, certs);
 							var docker = new Docker(dockerConfig);
