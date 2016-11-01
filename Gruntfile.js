1c02391f00b9574de499cd18ba93d37a6936d3b6
elianenassif <nassif.eliane@gmail.com>
2016-11-01 14:33:35 +0200

    minor update to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,8 @@ module.exports = function (grunt) {
 				// NODE_ENV: 'test',
 				// APP_DIR: process.cwd(),
 				APP_DIR_FOR_CODE_COVERAGE: '../test/coverage/instrument/',
-				SOAJS_SRVIP: '127.0.0.1'
+				SOAJS_SRVIP: '127.0.0.1',
+				SOAJS_ENV: 'dev'
 			}
 		},
 
