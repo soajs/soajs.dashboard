09097d2a6656e6869da7afce8ff4dd826a6bde0b
ameer <faraj.ameer@gmail.com>
2016-12-02 19:28:59 +0200

    Removed unneeded console log

--- ui/modules/dashboard/environments/services/hacloud.js
+++ ui/modules/dashboard/environments/services/hacloud.js
@@ -531,7 +531,6 @@ hacloudServices.service('hacloudSrv', ['ngDataApi', '$timeout', '$modal', functi
     }
 
     function deleteService(currentScope, serviceName, serviceVersion) {
-        console.log (serviceName);
         var params = {
             env: currentScope.envCode,
             name: serviceName,
