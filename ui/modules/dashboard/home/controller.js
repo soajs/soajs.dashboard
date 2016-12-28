ac96a19e05ee2fbbb8d3dec567223d448ec76246
mikehajj <mike.hajj@gmail.com>
2016-12-28 13:12:21 +0200

    added home page content

--- ui/modules/dashboard/home/controller.js
+++ ui/modules/dashboard/home/controller.js
@@ -1,8 +1,34 @@
 "use strict";
 var dahsboardApp = soajsApp.components;
-dahsboardApp.controller('dahsboardCtrl', ['$scope', 'injectFiles', function ($scope, injectFiles) {
+dahsboardApp.controller('dahsboardCtrl', ['$scope', '$timeout', 'injectFiles', function ($scope, $timeout, injectFiles) {
 	$scope.$parent.isUserLoggedIn();
 	injectFiles.injectCss('modules/dashboard/home/home.css');
+	
+	
+	(function(){
+		
+		var counter = 1;
+		var max = 4;
+		
+		jQuery("ul.slider li.p" + counter).css('display', 'block');
+		$timeout(function(){
+			showPillar();
+		}, 7000);
+		
+		function showPillar(){
+			jQuery("ul.slider li.p" + counter).fadeOut('slow', function(){
+				counter ++;
+				if(counter > max){
+					counter = 1;
+				}
+				jQuery("ul.slider li.p" + counter).fadeIn('slow', function(){
+					$timeout(function(){
+						showPillar();
+					}, 15000);
+				});
+			});
+		}
+	})();
 }]);
 
 dahsboardApp.controller('helpPageCtrl', ['$scope', function ($scope) {
