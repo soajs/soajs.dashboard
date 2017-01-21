8103382c43ac0214700b40f1fa7aa6acfa8616bc
mikehajj <mike.hajj@gmail.com>
2017-01-21 13:51:44 +0200

    FS-9: removed unneeded attributes from swagger directive, hooked dependency swaggerClient and performed code optimization to module controller. Overrode default swaggerClient service for module swaggerEditor only. added minor code assertion in main config.js

--- ui/modules/dashboard/swaggerEditor/controller.js
+++ ui/modules/dashboard/swaggerEditor/controller.js
@@ -1,7 +1,7 @@
 "use strict";
 var swaggerEditorApp = soajsApp.components;
 
-swaggerEditorApp.controller('swaggerEditorCtrl', ['$scope', '$timeout', 'injectFiles', 'swaggerEditorSrv', 'swaggerParser', function ($scope, $timeout, injectFiles, swaggerEditorSrv, swaggerParser) {
+swaggerEditorApp.controller('swaggerEditorCtrl', ['$scope', '$timeout', 'injectFiles', 'swaggerEditorSrv', 'swaggerParser', 'swaggerClient', function ($scope, $timeout, injectFiles, swaggerEditorSrv, swaggerParser, swaggerClient) {
 	$scope.$parent.isUserLoggedIn();
 	$scope.access = {};
 	$scope.schemaCode = '';
@@ -15,7 +15,9 @@ swaggerEditorApp.controller('swaggerEditorCtrl', ['$scope', '$timeout', 'injectF
 	
 	$scope.moveYamlRight = function(){
 		$scope.schemaCodeF = $scope.schemaCode;
-		watchSwaggerSimulator();
+		watchSwaggerSimulator(function(){
+			console.log("swagger ui info has been updated");
+		});
 	};
 	
 	$scope.aceLoaded = function(_editor){
@@ -30,16 +32,16 @@ swaggerEditorApp.controller('swaggerEditorCtrl', ['$scope', '$timeout', 'injectF
 	/*
 	 * This function updates the host value of the swagger simulator
 	 */
-	function watchSwaggerSimulator() {
+	function watchSwaggerSimulator(cb) {
 		//grab the swagger info
 		var x = swaggerParser.fetch();
 		if (!x || x.length === 0) {
 			$timeout(function () {
-				watchSwaggerSimulator();
+				watchSwaggerSimulator(cb);
 			}, 100);
 		}
 		else {
-			var dashboardDomain = apiConfiguration.domain.replace(window.location.protocol+"//", "");
+			var dashboardDomain = apiConfiguration.domain.replace(window.location.protocol + "//", "");
 			//modify the host value with the domain value of dashboard taken dynamically from the main config.js
 			x[3].host = dashboardDomain;
 			x[3].info.host = dashboardDomain;
@@ -49,16 +51,16 @@ swaggerEditorApp.controller('swaggerEditorCtrl', ['$scope', '$timeout', 'injectF
 			console.log("switching to host and basepath to swagger simulate api in dashboard:", x[3].host + x[3].basePath);
 			//apply the changes
 			swaggerParser.execute.apply(null, x);
+			return cb(null, true);
 		}
 	}
 	
 	$scope.GenerateService = function(){
 		if($scope.access.generate){
-			
+			//todo: do the get send data call to the generate api
+			//todo: add accept: application/zip
+			//todo: create blob from api response
 		}
-	};
-	$scope.TestValidApi = function() {
-		
 	};
 	
 	if($scope.access.generate){
