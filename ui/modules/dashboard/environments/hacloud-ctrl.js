993f60456c038bc97f9ce89eb30e620fb35bb21d
mikehajj <mike.hajj@gmail.com>
2017-02-06 11:54:08 +0200

    replaced refresh button with auto-refresh intervals dropdown menu.

--- ui/modules/dashboard/environments/hacloud-ctrl.js
+++ ui/modules/dashboard/environments/hacloud-ctrl.js
@@ -19,6 +19,73 @@ environmentsApp.controller('hacloudCtrl', ['$scope', '$cookies', '$timeout', 'no
 		}
 	};
 
+	$scope.refreshIntervals = [
+		{
+			v: 5,
+			l: '5 Seconds'
+		},
+		{
+			v: 10,
+			l: '10 Seconds'
+		},
+		{
+			v: 30,
+			l: '30 Seconds'
+		},
+		{
+			v: 60,
+			l: '1 Minute'
+		},
+		{
+			v: 300,
+			l: '5 Minutes'
+		},
+		{
+			v: 600,
+			l: '10 Minutes'
+		},
+		{
+			v: 1800,
+			l: '30 Minutes',
+			selected: true
+		},
+		{
+			v: 3600,
+			l: '1 Hour'
+		}
+	];
+	$scope.selectedInterval = {
+		v: 1800,
+		l: '30 Minutes',
+		selected: true
+	};
+	
+	if($cookies.getObject('selectedInterval')){
+		$scope.selectedInterval = $cookies.getObject('selectedInterval');
+	}
+	
+	$scope.changeInterval = function(oneInt){
+		$scope.refreshIntervals.forEach(function(oneInterval){
+			if(oneInterval.v === oneInt.v){
+				if(oneInt.v !== $scope.selectedInterval.v){
+					$scope.selectedInterval = oneInt;
+					$cookies.putObject('selectedInterval', oneInt);
+					//force reload autoRefresh
+					$scope.autoRefresh();
+				}
+			}
+		});
+	};
+	
+	$scope.autoRefresh = function(){
+		var tValue = $scope.selectedInterval.v * 1000;
+		$timeout(function(){
+			$scope.listServices(function(){
+				$scope.autoRefresh();
+			});
+		}, tValue);
+	};
+	
 	$scope.generateNewMsg = function (env, type, msg) {
 		$scope.waitMessage.type = type;
 		$scope.waitMessage.message = msg;
@@ -134,7 +201,9 @@ environmentsApp.controller('hacloudCtrl', ['$scope', '$cookies', '$timeout', 'no
 		$scope.checkCerts($scope.envCode);
 	}
 	if ($scope.access.listHosts) {
-		$scope.listServices();
+		$scope.listServices(function(){
+			$scope.autoRefresh();
+		});
 	}
 
 }]);
