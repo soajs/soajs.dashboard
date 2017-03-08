9826e8b1e360aa571a97db7bca0e2f2f69f6f803
RaghebAD <ragheb.abo.dargham@gmail.com>
2017-03-08 16:49:17 +0200

    update

--- utils/analytics/services/elk/elastic.js
+++ utils/analytics/services/elk/elastic.js
@@ -10,7 +10,9 @@ module.exports = {
 		"soajs.service.label": "elasticsearch"
 	},
 	"command": {
-		"cmd": ["/usr/share/elasticsearch/bin/plugin install delete-by-query; elasticsearch -Des.insecure.allow.root=true;"]
+	//	"cmd": ["/usr/share/elasticsearch/bin/plugin install delete-by-query; elasticsearch -Des.insecure.allow.root=true;"]
+		"cmd": ["elasticsearch"],
+		"args": ["-Des.insecure.allow.root=true"]
 	},
 	"deployConfig": {
 		"image": "elasticsearch:2.4.1",
