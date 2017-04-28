12ffe8b819e4a41e42f24d22ed7de07516750863
Peter <peter_eid@live.com>
2017-04-28 21:32:03 +0300

    fixed the Key

--- ui/config.js
+++ ui/config.js
@@ -16,8 +16,7 @@ var themeToUse = "default";
 var whitelistedDomain = ['localhost', '127.0.0.1', 'dashboard-api.' + mydomain];
 var apiConfiguration = {
 	domain: protocol + '//dashboard-api.' + mydomain,
-	key: "d44dfaaf1a3ba93adc6b3368816188f96134dfedec7072542eb3d84ec3e3d260f639954b8c0bc51e742c1dff3f80710e3e728edb004dce78d82d7ecd5e17e88c39fef78aa29aa2ed19ed0ca9011d75d9fc441a3c59845ebcf11f9393d5962549",
-	key_: '9b96ba56ce934ded56c3f21ac9bdaddc8ba4782b7753cf07576bfabcace8632eba1749ff1187239ef1f56dd74377aa1e5d0a1113de2ed18368af4b808ad245bc7da986e101caddb7b75992b14d6a866db884ea8aee5ab02786886ecf9f25e974'
+	key: "9b96ba56ce934ded56c3f21ac9bdaddc8ba4782b7753cf07576bfabcace8632eba1749ff1187239ef1f56dd74377aa1e5d0a1113de2ed18368af4b808ad245bc7da986e101caddb7b75992b14d6a866db884ea8aee5ab02786886ecf9f25e974"
 };
 
 var uiModuleDev = 'modules/dev';
