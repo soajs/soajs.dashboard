aa06d35d3d1fbff4c1e2e04883cd940412883eac
RaghebAD <ragheb.abo.dargham@gmail.com>
2017-05-03 13:36:17 +0300

    changed kibana image

--- utils/analytics/services/elk/kibana.js
+++ utils/analytics/services/elk/kibana.js
@@ -14,7 +14,7 @@ module.exports = {
 	},
 	"deployConfig": {
 		"version": "5.3.0",
-		"image": "kibana:5.3.0",
+		"image": "kibana-time-plugin",
 		"workDir": "/",
 		"memoryLimit": 209715200,
 		"network": "soajsnet",
