824b3a52352a04bcc1435b30c3db7836e15edbb1
nicolas@soajs.org <Ninobaros4>
2017-05-04 12:47:32 +0300

    bug fix

--- schemas/catalog.js
+++ schemas/catalog.js
@@ -118,7 +118,7 @@ module.exports = {
                                 "properties": {
                                     "deploy": {
                                         "type": "array",
-                                        "required": false,
+                                        "required": true,
                                         "items": { "type": "string", "required": false }
                                     }
                                 }
