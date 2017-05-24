ec6ac13e5fe051e8d2e554f8f14b0fae60d9bb96
ameer <faraj.ameer@gmail.com>
2017-05-24 12:19:39 +0300

    Removed restriction on catalog type in catalog validator

--- schemas/catalog.js
+++ schemas/catalog.js
@@ -8,7 +8,7 @@ module.exports = {
         "properties": {
             "name": { "type": "string", "required": true },
             "locked": { "type": "boolean", "required": false },
-            "type": { "type": "string", "required": true, "enum": [ "soajs", "nginx", "nodejs", "mongo", "es" ] },
+            "type": { "type": "string", "required": true },
             "subtype": { "type": "string", "required": false, "enum": [ "service", "daemon" ] },
             "description": { "type": "string", "required": true },
             "recipe": {
