68cfd1951fe90da322567e4a5d5abbb59fd1178c
mikehajj <mike.hajj@gmail.com>
2017-05-25 17:08:08 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1205,7 +1205,7 @@ service.init(function () {
      */
     service.put("/cd/action", function (req, res) {
         initBLModel(req, res, cdBL, dbModel, function (BL) {
-            BL.cdAction(config, req, function (error, data) {
+            BL.cdAction(config, service.registry, req, function (error, data) {
                 return res.jsonp(req.soajs.buildResponse(error, data));
             });
         });
