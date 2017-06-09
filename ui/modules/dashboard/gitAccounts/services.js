10fadf573184e7183e1de6455b7b0f013d0699f5
mikehajj <mike.hajj@gmail.com>
2017-06-09 18:40:25 +0300

    bug fix

--- ui/modules/dashboard/gitAccounts/services.js
+++ ui/modules/dashboard/gitAccounts/services.js
@@ -59,7 +59,7 @@ repoService.service('repoSrv', ['ngDataApi', '$timeout', '$modal', '$cookies', f
 				$scope.activateRepo = false;
 				if(ciRepo) {
                     $scope.ciRepoName = ciRepo.name;
-					$scope.activateRepo = true;
+					$scope.activateRepo = false;
                 }
 				$scope.goTOCI = function(){
 					currentScope.$parent.go('#/continuous-integration');
