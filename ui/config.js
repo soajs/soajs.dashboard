3a03c77738a79efe87ffb9a706950d19247c9dc8
mikehajj <mike.hajj@gmail.com>
2017-07-10 11:29:13 +0300

    updated soajs rms list

--- ui/config.js
+++ ui/config.js
@@ -17,7 +17,7 @@ var apiConfiguration = {
 	key: '9b96ba56ce934ded56c3f21ac9bdaddc8ba4782b7753cf07576bfabcace8632eba1749ff1187239ef1f56dd74377aa1e5d0a1113de2ed18368af4b808ad245bc7da986e101caddb7b75992b14d6a866db884ea8aee5ab02786886ecf9f25e974'
 };
 
-var SOAJSRMS = ['controller','urac','oauth','dashboard','proxy', 'gcs'];
+var SOAJSRMS = ['soajs.controller','soajs.urac','soajs.oauth','soajs.dashboard','soajs.prx'];
 
 var uiModuleDev = 'modules/dev';
 var uiModuleStg = 'modules/stg';
