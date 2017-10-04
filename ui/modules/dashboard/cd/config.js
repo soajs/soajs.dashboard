8aaa5ee9abc8e3a105d0441b91e5eb3ea1d09673
mikehajj <mike.hajj@gmail.com>
2017-10-04 12:18:40 +0300

    removed ui folder.

--- ui/modules/dashboard/cd/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var cdAppConfig = {
-	permissions: {
-		get: ['dashboard', '/cd', 'get'],
-		save: ['dashboard', '/cd', 'post'],
-		pause: ['dashboard', '/cd/pause', 'post']
-	},
-	upgradeSpaceLink : "https://soajsorg.atlassian.net/wiki/spaces/UU"
-};
