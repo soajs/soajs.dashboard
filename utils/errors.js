38ec8d66ea0370835a63b615d0ba2960805a68c2
charleskassouf <charles.kassouf11@gmail.com>
2018-03-23 12:21:41 +0200

    update for catalog recipes

--- utils/errors.js
+++ utils/errors.js
@@ -270,6 +270,7 @@ var errors = {
 	"922": "Please provide either a schema or a swagger object",
 	"923": "Warning! Publish failed. You don't have any api.",
 
+    "949": "Must specify the type for docker volume",
 	"950": "Catalog recipe not found",
 	"951": "You are not allowed to edit or delete a locked recipe",
 	"952": "Unable to update catalog recipe",
