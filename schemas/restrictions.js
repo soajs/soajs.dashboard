770f98ca27be72718e89039126fe0e44ed3b7c2b
RaghebAD <ragheb.abo.dargham@gmail.com>
2018-10-10 13:32:48 +0300

    update to HER-1085

--- schemas/restrictions.js
+++ schemas/restrictions.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const soajsUtils = require("soajs.core.libs").utils;
 let manual = {
 	"type": "object",
 	"required": true,
@@ -55,32 +55,32 @@ let manual = {
 						"recipes": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"productization": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"daemonGroups": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"tenant": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"custom_registry": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"deployments": {
 							"type": "object",
 							"required": false,
-							"properties:": {
+							"properties": {
 								"resources": {
 									"type": "object",
 									"patternProperties": {
@@ -147,21 +147,8 @@ let container = {
 				"deploy": {
 					"type": "object",
 					"required": true,
-					"additionalProperties": false,
+					"additionalProperties": true,
 					"properties": {
-						"deployment": {
-							"type": "object",
-							"required": true,
-							"additionalProperties": false,
-							"properties": {
-								"container": {
-									"type": "object",
-									"required": true,
-									"additionalProperties": true,
-									"properties": {}
-								}
-							}
-						},
 						"selectedDriver": {
 							"type": "string",
 							"required": true,
@@ -184,246 +171,47 @@ let container = {
 						"recipes": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"productization": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"tenant": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"custom_registry": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"endpoints": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"secrets": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"daemonGroups": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"iac": {
 							"type": "object",
 							"required": false,
-							"properties:": {}
+							"properties": {}
 						},
 						"deployments": {
 							"type": "object",
 							"required": false,
-							"properties:": {
-								"resources": {
-									"type": "object",
-									"patternProperties": {
-										"\w+": {
-											"type": "object",
-											"required": false,
-											"properties": {
-												"label": {"type": "string", "required": true},
-												"type": {"type": "string", "required": true},
-												"category": {"type": "string", "required": true},
-												"ui": {"type": "string", "required": true},
-												"locked": {"type": "string", "required": false},
-												"plugged": {"type": "string", "required": false},
-												"shared": {"type": "string", "required": false},
-												"config": {"type": "object", "required": false},
-												"limit": {"type": "number", "min": 0, "required": false},
-												"deploy": {
-													"type": "object",
-													"required": false,
-													"properties:": {
-														"restriction": {
-															"type": "object",
-															"required": false,
-															"properties": {
-																"type": {
-																	"type": "string",
-																	"required": false,
-																	"enum": ["container"]
-																},
-																"technology": {
-																	"type": "string",
-																	"required": false,
-																	"enum": ["docker", "kubernetes"]
-																}
-															}
-														}
-													}
-												}
-											},
-											"additionalProperties": false
-										}
-									}
-								},
-								"repo": {
-									"type": "object",
-									"patternProperties": {
-										"\w+": {
-											"type": "object",
-											"required": false,
-											"properties": {
-												"label": {"type": "string", "required": true},
-												"type": {"type": "string", "required": true},
-												"category": {"type": "string", "required": true},
-												"gitSource": {"type": "object", "required": true},
-												"limit": {"type": "number", "min": 0, "required": false},
-												"deploy": {
-													"type": "object",
-													"required": false,
-													"properties:": {
-														"restriction": {
-															"type": "object",
-															"required": false,
-															"properties": {
-																"type": {
-																	"type": "string",
-																	"required": false,
-																	"enum": ["container"]
-																},
-																"technology": {
-																	"type": "string",
-																	"required": false,
-																	"enum": ["docker", "kubernetes"]
-																}
-															}
-														}
-													}
-												}
-											},
-											"additionalProperties": false
-										}
-									}
-								}
-							}
-						}
-					}
-				},
-				"deploy": {
-					"type": "object",
-					"required": false,
-					"additionalProperties": false,
-					"properties": {}
-				},
-				"restrictions": {
-					"type": "object",
-					"required": false,
-					"properties": {
-						"deployment": {
-							"type": "array",
-							"items": {
-								"type": "string",
-								"enum": ["container"]
-							},
-							"required": false,
-							"uniqueItems": true
-						}
-					},
-					"additionalProperties": true
-				}
-			}
-		}
-	},
-	"additionalProperties": false
-};
-
-let singleInfra = {
-	"type": "object",
-	"required": true,
-	"properties": {
-		"data": {
-			"type": "object",
-			"required": false,
-			"additionalProperties": true,
-			"properties": {
-				"deploy": {
-					"type": "object",
-					"required": true,
-					"additionalProperties": false,
-					"properties": {
-						"deployment": {
-							"type": "object",
-							"required": true,
-							"additionalProperties": false,
 							"properties": {
-							
-							}
-						},
-						"selectedDriver": {
-							"type": "string",
-							"required": true,
-							"enum": ["vm"]
-						}
-					}
-				},
-			}
-		},
-		"template": {
-			"type": "object",
-			"required": true,
-			"additionalProperties": false,
-			"properties": {
-				"content": {
-					"type": "object",
-					"required": false,
-					"additionalProperties": false,
-					"properties": {
-						"recipes": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"productization": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"tenant": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"custom_registry": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"endpoints": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"secrets": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"daemonGroups": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"iac": {
-							"type": "object",
-							"required": false,
-							"properties:": {}
-						},
-						"deployments": {
-							"type": "object",
-							"required": false,
-							"properties:": {
 								"resources": {
 									"type": "object",
 									"patternProperties": {
@@ -443,7 +231,7 @@ let singleInfra = {
 												"deploy": {
 													"type": "object",
 													"required": false,
-													"properties:": {
+													"properties": {
 														"restriction": {
 															"type": "object",
 															"required": false,
@@ -451,7 +239,12 @@ let singleInfra = {
 																"type": {
 																	"type": "string",
 																	"required": false,
-																	"enum": ["vm"]
+																	"enum": ["container"]
+																},
+																"technology": {
+																	"type": "string",
+																	"required": false,
+																	"enum": ["docker", "kubernetes"]
 																}
 															}
 														}
@@ -477,7 +270,7 @@ let singleInfra = {
 												"deploy": {
 													"type": "object",
 													"required": false,
-													"properties:": {
+													"properties": {
 														"restriction": {
 															"type": "object",
 															"required": false,
@@ -485,7 +278,12 @@ let singleInfra = {
 																"type": {
 																	"type": "string",
 																	"required": false,
-																	"enum": ["vm"]
+																	"enum": ["container"]
+																},
+																"technology": {
+																	"type": "string",
+																	"required": false,
+																	"enum": ["docker", "kubernetes"]
 																}
 															}
 														}
@@ -508,25 +306,286 @@ let singleInfra = {
 				},
 				"restrictions": {
 					"type": "object",
-					"required": false,
+					"required": true,
 					"properties": {
 						"deployment": {
 							"type": "array",
 							"items": {
 								"type": "string",
-								"enum": ["vm"]
+								"enum": ["container"]
 							},
 							"required": false,
 							"uniqueItems": true
 						}
 					},
-					"additionalProperties": true
+					"driver": {
+						"type": "array",
+						"items": {
+							"type": "string" //container.docker | container.kubernetes ....
+						},
+						"required": false,
+						"uniqueItems": true
+					},
+					"allowInfraReuse": {
+						"type": "boolean"
+					},
+					"additionalProperties": false
 				}
 			}
 		}
 	},
 	"additionalProperties": false
 };
+
+let imfv =  {
+	"data": {
+		"type": "object",
+		"required": false,
+		"additionalProperties": true,
+		"properties": {
+			"deploy": {
+				"type": "object",
+				"required": true,
+				"additionalProperties": true,
+				"properties": {
+					"selectedDriver": {
+						"type": "string",
+						"required": true,
+						"enum": ["vm", "container"]
+					}
+				}
+			},
+		}
+	},
+	"template": {
+		"type": "object",
+		"required": true,
+		"additionalProperties": true,
+		"properties": {
+			"restrictions": {
+				"type": "object",
+				"required": true,
+				"properties": {
+					"deployment": {
+						"type": "array",
+						"items": {
+							"type": "string",
+							"enum": []
+						},
+						"required": false,
+						"uniqueItems": true
+					}
+				},
+				"driver": {
+					"type": "array",
+					"items": {
+						"type": "string"
+					},
+					"required": false,
+					"uniqueItems": true
+				},
+				"allowInfraReuse": {
+					"type": "boolean"
+				},
+				"infra": {
+					"type": "array",
+					"items": {
+						"type": "string"
+					},
+					"required": true,
+					"uniqueItems": true
+				},
+				"additionalProperties": false
+			},
+			"content": {
+				"type": "object",
+				"required": false,
+				"additionalProperties": false,
+				"properties": {
+					"recipes": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"productization": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"tenant": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"custom_registry": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"endpoints": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"secrets": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"daemonGroups": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"iac": {
+						"type": "object",
+						"required": false,
+						"properties": {}
+					},
+					"deployments": {
+						"type": "object",
+						"required": false,
+						"properties": {
+							"resources": {
+								"type": "object",
+								"patternProperties": {
+									"\w+": {
+										"type": "object",
+										"required": false,
+										"properties": {
+											"label": {"type": "string", "required": true},
+											"type": {"type": "string", "required": true},
+											"category": {"type": "string", "required": true},
+											"ui": {"type": "string", "required": true},
+											"locked": {"type": "string", "required": false},
+											"plugged": {"type": "string", "required": false},
+											"shared": {"type": "string", "required": false},
+											"config": {"type": "object", "required": false},
+											"limit": {"type": "number", "min": 0, "required": false},
+											"deploy": {
+												"type": "object",
+												"required": false,
+												"properties": {
+													"restriction": {
+														"type": "object",
+														"required": false,
+														"properties": {
+															"type": {
+																"type": "string",
+																"required": false,
+																"enum": []
+															}
+														}
+													}
+												}
+											}
+										},
+										"additionalProperties": false
+									}
+								}
+							},
+							"repo": {
+								"type": "object",
+								"patternProperties": {
+									"\w+": {
+										"type": "object",
+										"required": false,
+										"properties": {
+											"label": {"type": "string", "required": true},
+											"type": {"type": "string", "required": true},
+											"category": {"type": "string", "required": true},
+											"gitSource": {"type": "object", "required": true},
+											"limit": {"type": "number", "min": 0, "required": false},
+											"deploy": {
+												"type": "object",
+												"required": false,
+												"properties": {
+													"restriction": {
+														"type": "object",
+														"required": false,
+														"properties": {
+															"type": {
+																"type": "string",
+																"required": false,
+																"enum": []
+															}
+														}
+													}
+												}
+											}
+										},
+										"additionalProperties": false
+									}
+								}
+							}
+						}
+					}
+				}
+			},
+			"deploy": {
+				"type": "object",
+				"required": false,
+				"additionalProperties": true,
+				"properties": {}
+			}
+		}
+	}
+};
+let options = ["vm", "container", "composite", null, "array"];
+let singleInfra = {
+	"type": "object",
+	"required": true,
+	"oneOf": []
+};
+options.forEach((opt)=>{
+	switch (opt) {
+		case "vm":
+			imfv.template.properties.restrictions.properties.deployment.items.enum = ["vm"];
+			imfv.template.properties.content.properties.deployments.properties.resources.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["vm"];
+			imfv.template.properties.content.properties.deployments.properties.repo.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["vm"];
+			singleInfra.oneOf.push({
+				"properties": soajsUtils.cloneObj(imfv),
+				"additionalProperties": false
+			});
+			break;
+		case "container":
+			imfv.template.properties.restrictions.properties.deployment.items.enum = ["container"];
+			imfv.template.properties.content.properties.deployments.properties.resources.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container"];
+			imfv.template.properties.content.properties.deployments.properties.repo.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container"];
+			singleInfra.oneOf.push({
+				"properties": soajsUtils.cloneObj(imfv),
+				"additionalProperties": false
+			});
+			break;
+		case "composite":
+			imfv.template.properties.restrictions.properties.deployment.items.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.resources.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.repo.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container", "vm"];
+			singleInfra.oneOf.push({
+				"properties": soajsUtils.cloneObj(imfv),
+				"additionalProperties": false
+			});
+			break;
+		case "array":
+			imfv.template.properties.restrictions.properties.deployment.items.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.resources.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.repo.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container, \"vm\""];
+			singleInfra.oneOf.push({
+				"properties": soajsUtils.cloneObj(imfv),
+				"additionalProperties": false
+			});
+			break;
+		case null:
+			imfv.template.properties.restrictions.properties.deployment.items.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.resources.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container", "vm"];
+			imfv.template.properties.content.properties.deployments.properties.repo.patternProperties["\w+"].properties.deploy.properties.restriction.properties.type.enum = ["container", "vm"];
+			singleInfra.oneOf.push({
+				"properties": soajsUtils.cloneObj(imfv),
+				"additionalProperties": false
+			});
+			break;
+	}
+});
 
 module.exports = {
 	"manual": manual,
