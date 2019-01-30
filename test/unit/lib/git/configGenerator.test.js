2f544fadc97751f836d4c219de54b2d513e19e9e
RaghebAD <ragheb.abo.dargham@gmail.com>
2019-01-30 19:47:56 +0200

    internal change in activating a repository and some bug fixes

--- test/unit/lib/git/configGenerator.test.js
+++ test/unit/lib/git/configGenerator.test.js
@@ -8,70 +8,60 @@ describe("testing helper git.js", function () {
        var configPath = "bad path";
        var yamlPath = __dirname + "/../../../uploads/generateConfigFiles/badYamlFile.yml";
         helpers.generate(configPath, yamlPath, function(err, res){
-            assert.ok(err);
-            assert.deepEqual(err, {
-                "code": 983,
-                "msg": "Bad syntax found in the config file."
-            });
+            assert.ok(res);
+            done()
         });
-        done();
-    });
-
-    it("Fail - Cannot parse Yaml file", function(done){
-        var configPath = __dirname + "/../../../uploads/generateConfigFiles/configFile.js";
-        var yamlPath =  __dirname + "/../../../uploads/valid.tar";
-        helpers.generate(configPath, yamlPath, function(err, res){
-            assert.ok(err);
-            assert.deepEqual(err, {
-                code: 984,
-                msg: 'Unable to parse.'
-            });
-        });
-        done();
-    });
-
-    it("Fail - Bad Yaml file", function(done){
-        var configPath = __dirname + "/../../../uploads/generateConfigFiles/configFile.js";
-        var yamlPath = __dirname + "/../../../uploads/generateConfigFiles/badYamlFile.yml";
-        helpers.generate(configPath, yamlPath, function(err, res){
-            assert.ok(err);
-            assert.deepEqual(err, {
-                code: 985,
-                msg: 'Yaml file was converted to a string'
-            });
-        });
-        done();
     });
 
     it("Fail - Empty Yaml file", function(done){
-        var configPath = __dirname + "/../../../uploads/generateConfigFiles/configFile.js";
-        var yamlPath = __dirname + "/../../../uploads/generateConfigFiles/emptyYamlFile.yml";
-        helpers.generate(configPath, yamlPath, function(err, res){
-            assert.ok(err);
-            assert.deepEqual(err, {
-                code: 985,
-                msg: 'Cannot read property \'paths\' of null'
-            });
+        var configPath = { type: 'service',
+	        prerequisites: { cpu: ' ', memory: ' ' },
+	        swagger: true,
+	        swaggerFilename: 'swagger.yml',
+	        serviceName: 'express',
+	        serviceGroup: 'Custom Services',
+	        serviceVersion: 1,
+	        servicePort: 4381,
+	        requestTimeout: 30,
+	        requestTimeoutRenewal: 5,
+	        extKeyRequired: true,
+	        oauth: true,
+	        session: false,
+	        urac: false,
+	        urac_Profile: false,
+	        urac_ACL: false,
+	        provision_ACL: false,
+	        errors: {},
+	        schema: {} };
+        helpers.generate(configPath, null, function(err, res){
+            assert.ok(res);
+	        done();
         });
-        done();
-    });
-
-    it("Fail - No summary for API", function(done){
-        var configPath = __dirname + "/../../../uploads/generateConfigFiles/configFile.js";
-        var yamlPath = __dirname + "/../../../uploads/generateConfigFiles/noSummary.yml";
-        helpers.generate(configPath, yamlPath, function(err, res){
-           assert.ok(err);
-            assert.deepEqual(err, {
-                code: 985,
-                msg: 'Please enter a summary for API oneMethod: onePath you want to build.'
-            });
-        });
-        done();
     });
 
     it("Success - config file generated", function(done){
-        var configPath = __dirname + "/../../../uploads/generateConfigFiles/configFile.js";
-        var yamlPath = __dirname + "/../../../uploads/generateConfigFiles/validYamlFile.yml";
+        var configPath = { type: 'service',
+	        prerequisites: { cpu: ' ', memory: ' ' },
+	        swagger: true,
+	        swaggerFilename: 'swagger.yml',
+	        serviceName: 'express',
+	        serviceGroup: 'Custom Services',
+	        serviceVersion: 1,
+	        servicePort: 4381,
+	        requestTimeout: 30,
+	        requestTimeoutRenewal: 5,
+	        extKeyRequired: true,
+	        oauth: true,
+	        session: false,
+	        urac: false,
+	        urac_Profile: false,
+	        urac_ACL: false,
+	        provision_ACL: false,
+	        errors: {},
+	        schema: {} };
+        var yamlPath = { token: null,
+	        downloadLink: 'https://raw.githubusercontent.com/RaghebAD/soajs.nodejs.express/master/swagger.yml',
+	        content: 'swagger: "2.0"\ninfo:\n  version: "1.0.0"\n  title: express demo\nhost: dev-api.mydomain.com\nbasePath: /express\nschemes:\n  - http\npaths:\n  /tidbit/hello:\n    get:\n      tags:\n        - hello\n      summary: Hello World\n      parameters:\n        - name: username\n          in: query\n          required: true\n          type: string\n        - name: lastname\n          in: query\n          required: true\n          type: string\n      responses:\n        200:\n          description: successful operation\n          schema:\n            type: object\n    post:\n      tags:\n        - hello\n      summary: Returns SOAJS Object\n      responses:\n        200:\n          description: successful operation\n          schema:\n            type: object' };
         helpers.generate(configPath, yamlPath, function(err, res){
             assert.ok(res)
         });
