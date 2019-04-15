0c99165e2e1fb6b931a15fcaf1bc877f9bdaefed
RaghebAD <ragheb.abo.dargham@gmail.com>
2019-04-15 15:17:07 -0300

    increased default branches to 100

--- utils/drivers/git/github/helper.js
+++ utils/drivers/git/github/helper.js
@@ -159,7 +159,8 @@ var lib = {
 			}
 			github.repos.getBranches({
 				user: repoInfo[ 0 ],
-				repo: repoInfo[ 1 ]
+				repo: repoInfo[ 1 ],
+				per_page: 100
 			}, function (error, response) {
 				checkIfError(error && error.message && error.message.indexOf('API rate limit exceeded') !== -1, {
 					code: 776,
@@ -170,6 +171,10 @@ var lib = {
 						message: 'Repository not found'
 					}, cb, function () {//in case of invalid repo info
 						checkIfError(error, { code: 763 }, cb, function () {//generic case
+							//todo add support for
+							//multiple page branches
+							//find a way to parse the link found in response.meta
+							// link: '<https://api.github.com/repositories/222/branches?per_page=10&access_token=2222&page=2>; rel="next", <https://api.github.com/repositories/222/branches?per_page=10&access_token=2222&page=4>; rel="last"',
 							return cb(null, response);
 						});
 					});
