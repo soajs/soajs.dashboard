da80b8681cd233fc8e2c8e4acc54eea371dd2414
Ragheb Bou Dargham <ragheb.abo.dargham@gmail.com>
2019-05-14 00:57:19 +0300

    removed unneeded code

--- test/unit/lib/cloud/infra/index.test.js
+++ test/unit/lib/cloud/infra/index.test.js
@@ -529,8 +529,6 @@ describe("testing lib/cloud/infra/index.js", function () {
 		it("Success downloadTemplate", function (done) {
 			var res = {};
 			infra.downloadTemplate(config, req.soajs, deployer, res, function (error, body) {
-				console.log(error)
-				console.log(body)
 				done();
 			});
 		});
@@ -542,9 +540,6 @@ describe("testing lib/cloud/infra/index.js", function () {
 		it("Success onboardVM", function (done) {
 			req.soajs.inputmaskData.env = "dev";
 			infra.onboardVM(config, req, req.soajs, deployer, function (error, body) {
-				console.log("?????????")
-				console.log(error)
-				console.log(body)
 				done();
 			});
 		});
