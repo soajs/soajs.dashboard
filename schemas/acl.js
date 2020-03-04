ef316260167ba95b9cce0332807a6ca25aecfc04
Ragheb Bou Dargham <ragheb.abo.dargham@gmail.com>
2020-03-04 17:14:13 +0200

    fixed json validator for acl when updating a package

--- schemas/acl.js
+++ schemas/acl.js
@@ -1,10 +1,4 @@
 "use strict";
-var accessSchema = {
-	"oneOf": [
-		{"type": "boolean", "required": false},
-		{"type": "array", "minItems": 1, "items": {"type": "string", "required": true}, "required": false}
-	]
-};
 
 var apisObject = {
 	"type": "object",
@@ -14,9 +8,9 @@ var apisObject = {
 			"type": "object",
 			"required": true,
 			"properties": {
-				"access": accessSchema
-			},
-			"additionalProperties": false
+				"access": {"type": "boolean", "required": false},
+				"group:": {"type": "string", "required": false}
+			}
 		}
 	}
 };
@@ -28,46 +22,64 @@ var aclMethod = {
 		"apis": apisObject
 	}
 };
-
 var acl = {
 	'source': ['body.acl'],
 	'required': false,
 	'validation': {
 		"type": "object",
 		"patternProperties": {
-			"^[a-zA-Z0-9]{4}$": {
+			"^[a-zA-Z0-9]{3,4}$": {
 				"type": "object",
-				"additionalProperties": {
-					"type": "object",
-					"required": false,
-					"properties": {
-						"access": accessSchema,
-						"apisPermission": {
-							"type": "string", "enum": ["restricted"], "required": false
-						},
-						"apis": apisObject,
-						"get": aclMethod,
-						"post": aclMethod,
-						"put": aclMethod,
-						"delete": aclMethod,
-						"apisRegExp": {
-							"type": "array",
-							"required": false,
-							"minItems": 1,
-							"items": {
+				"patternProperties": {
+					"^[^\W\.]+$": {
+						"type": "object",
+						"required": false,
+						"patternProperties": {
+							".+": {
 								"type": "object",
+								"required": false,
 								"properties": {
-									"regExp": {"type": "pattern", required: true, "pattern": /\.+/},
-									"access": accessSchema
+									"access": {"type": "boolean", "required": false},
+									"apisPermission": {
+										"type": "string", "enum": ["restricted"], "required": false
+									},
+									"apis": apisObject,
+									"get": aclMethod,
+									"post": aclMethod,
+									"put": aclMethod,
+									"delete": aclMethod,
+									"head": aclMethod,
+									"options": aclMethod,
+									"other": aclMethod,
+									"apisRegExp": {
+										"type": "array",
+										"required": false,
+										"minItems": 1,
+										"items": {
+											"type": "object",
+											"properties": {
+												"regExp": {
+													"type": "pattern",
+													"required": true,
+													"pattern": /\.+/
+												},
+												"access": {"type": "boolean", "required": false}
+											},
+											"additionalProperties": false
+										}
+									},
+									"additionalProperties": false
 								},
 								"additionalProperties": false
 							}
-						}
-					},
-					"additionalProperties": false
-				}
+						},
+						"additionalProperties": false
+					}
+				},
+				"additionalProperties": false
 			}
-		}
+		},
+		"additionalProperties": false
 	}
 };
 
